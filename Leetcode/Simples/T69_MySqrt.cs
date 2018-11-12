bbe1417976c17941d93037f9f0c08715ccc0184f
azengzz <546800691@qq.com>
2018-11-13 06:59:01 +0800

    T69: implement int sqrt(int x)

---
+++ Leetcode/Simples/T69_MySqrt.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Simples
+{
+    /*
+     * Implement int sqrt(int x).
+     * Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+     * Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+     * 
+     * Example 1:
+     * Input: 4
+     * Output: 2
+     * 
+     * Example 2:
+     * Input: 8
+     * Output: 2
+     * Explanation: The square root of 8 is 2.82842..., and since 
+     * the decimal part is truncated, 2 is returned.
+     */
+
+    public class T69_MySqrt
+    {
+        public T69_MySqrt()
+        {
+        }
+
+        public int MySqrt(int x)  //!要考虑数字太大时溢出的问题！
+        {
+            if (x < 2) return x;
+
+            int left = 0, right = x;
+            int middle = 0;
+
+            while (left != right )
+            {
+                middle = (left + right) / 2;
+                if (middle > x / middle)    //直接middle * middle可能会溢出
+                {
+                    right = middle;
+                }
+                else if (middle < x / middle)
+                {
+                    left = middle;
+                }
+                else break;
+
+                if ((left + 1 == right) && (left < x / left) && (right > x / right))
+                {
+                    middle = left;
+                    left = right;
+                    break;
+                }
+            } 
+
+            return middle;
+        }
+    }
+}
---
+++ LeetcodeTests/Simples/T69_MySqrtTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.Simples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Simples.Tests
+{
+    [TestClass()]
+    public class T69_MySqrtTests
+    {
+        T69_MySqrt t69 = new T69_MySqrt();
+
+        [TestMethod()]
+        public void MySqrtTest_input_4()
+        {
+            int input = 4;
+            int expect = 2;
+            int result = t69.MySqrt(input);
+            Assert.IsTrue(result == expect);
+        }
+
+        [TestMethod()]
+        public void MySqrtTest_input_9()
+        {
+            int input = 9;
+            int expect = 3;
+            int result = t69.MySqrt(input);
+            Assert.IsTrue(result == expect);
+        }
+
+        [TestMethod()]
+        public void MySqrtTest_input_8()
+        {
+            int input = 8;
+            int expect = 2;
+            int result = t69.MySqrt(input);
+            Assert.IsTrue(result == expect);
+        }
+
+        [TestMethod()]
+        public void MySqrtTest_input_17()
+        {
+            int input = 17;
+            int expect = 4;
+            int result = t69.MySqrt(input);
+            Assert.IsTrue(result == expect);
+        }
+
+        [TestMethod()]
+        public void MySqrtTest_input_0()
+        {
+            int input = 0;
+            int expect = 0;
+            int result = t69.MySqrt(input);
+            Assert.IsTrue(result == expect);
+        }
+
+        [TestMethod()]
+        public void MySqrtTest_input_1()
+        {
+            int input = 1;
+            int expect =1;
+            int result = t69.MySqrt(input);
+            Assert.IsTrue(result == expect);
+        }
+
+        [TestMethod()]
+        public void MySqrtTest_input_65536()
+        {
+            int input = 65536;
+            int expect = 256;
+            int result = t69.MySqrt(input);
+            Assert.IsTrue(result == expect);
+        }
+       
+        [TestMethod()]
+        public void MySqrtTest_input_very_large()
+        {
+            int input = 2147395599;
+            int expect = 46339;
+            int result = t69.MySqrt(input);
+            Assert.IsTrue(result == expect);
+        }
+    }
+}
