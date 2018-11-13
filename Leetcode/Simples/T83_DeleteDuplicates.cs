ec4de666639222dd21d151a005162eadea21d5fd
azengzz <546800691@qq.com>
2018-11-13 23:41:16 +0800

    t81 delete duplicate

---
+++ Leetcode/Simples/T83_DeleteDuplicates.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leetcode;
+
+namespace Leetcode.Simples
+{
+    public class T83_DeleteDuplicates
+    {
+        /*
+            Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+            Example 1:
+            Input: 1->1->2
+            Output: 1->2
+
+            Example 2:
+            Input: 1->1->2->3->3
+            Output: 1->2->3
+         */
+
+        public T83_DeleteDuplicates()
+        { }
+
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null) return null;
+
+            ListNode slowPtr = head, fastPtr = head.next;
+            while (fastPtr != null)
+            {
+                if (fastPtr.val == slowPtr.val)
+                {
+                    ListNode deletedPtr = fastPtr;
+                    fastPtr = fastPtr.next;
+                    slowPtr.next = fastPtr;
+                    deletedPtr.next = null;
+                }
+                else
+                {
+                    fastPtr = fastPtr.next;
+                    slowPtr = slowPtr.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
+
+
+/*
+ 更加简单的解答：
+ public ListNode DeleteDuplicates(ListNode head)
+{
+    ListNode current = head;
+    while (current != null && current.next != null)
+    { 
+        if (current.val == current.next.val)
+        {
+            current.next = current.next.next;
+        }
+        else
+        {
+            current = current.next;
+        }
+    }
+
+    return head;
+}
+ */
---
+++ LeetcodeTests/Simples/T83_DeleteDuplicatesTests.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.Simples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leetcode;
+
+namespace Leetcode.Simples.Tests
+{
+    [TestClass()]
+    public class T83_DeleteDuplicatesTests
+    {
+        private ListNode CreateLinkedListByArray(int[] elements)
+        {
+            ListNode node0 = new ListNode(elements[0]) { next = null };
+            ListNode head = node0;
+            ListNode walk = head;
+            for (int i = 1; i < elements.Length; i++)
+            {
+                ListNode node = new ListNode(elements[i]) { next = null };
+                walk.next = node;
+                walk = walk.next;
+            }
+            return head;
+        }
+
+        private int[] GetElementsFromLinkedList(ListNode head)
+        {
+            List<int> elements = new List<int>();
+            while(head != null)
+            {
+                elements.Add(head.val);
+                head = head.next;
+            }
+            return elements.ToArray();
+        }
+
+        private bool CompareArrays(int[] arr1, int[] arr2)
+        {
+            if (arr1.Length != arr2.Length) return false;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != arr2[i]) return false;
+            }
+            return true;
+        }
+
+        T83_DeleteDuplicates t83 = new T83_DeleteDuplicates();
+
+        [TestMethod()]
+        public void DeleteDuplicatesTest_1()
+        {
+            int[] list = { 1, 1, 2 };
+            ListNode head = CreateLinkedListByArray(list);
+            t83.DeleteDuplicates(head);
+            int[] result = GetElementsFromLinkedList(head);
+            int[] expect = { 1, 2 };
+            Assert.IsTrue(CompareArrays(result, expect));
+        }
+
+        [TestMethod()]
+        public void DeleteDuplicatesTest_2()
+        {
+            int[] list = { 1, 1, 2, 3, 3 };
+            ListNode head = CreateLinkedListByArray(list);
+            t83.DeleteDuplicates(head);
+            int[] result = GetElementsFromLinkedList(head);
+            int[] expect = { 1, 2, 3 };
+            Assert.IsTrue(CompareArrays(result, expect));
+        }
+
+        [TestMethod()]
+        public void DeleteDuplicatesTest_3()
+        {
+            int[] list = { 1, 1, 1, 2, 2, 3, 3, 3, 5 };
+            ListNode head = CreateLinkedListByArray(list);
+            t83.DeleteDuplicates(head);
+            int[] result = GetElementsFromLinkedList(head);
+            int[] expect = { 1, 2, 3, 5 };
+            Assert.IsTrue(CompareArrays(result, expect));
+        }
+
+        [TestMethod()]
+        public void DeleteDuplicatesTest_null()
+        {
+            ListNode head = null;
+            t83.DeleteDuplicates(head);
+            int[] result = GetElementsFromLinkedList(head);
+            int[] expect = { };
+            Assert.IsTrue(CompareArrays(result, expect));
+        }
+
+        [TestMethod()]
+        public void DeleteDuplicatesTest_single()
+        {
+            int[] list = { 1 };
+            ListNode head = CreateLinkedListByArray(list);
+            t83.DeleteDuplicates(head);
+            int[] result = GetElementsFromLinkedList(head);
+            int[] expect = { 1 };
+            Assert.IsTrue(CompareArrays(result, expect));
+        }
+    }
+}
