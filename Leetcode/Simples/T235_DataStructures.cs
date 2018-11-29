90e9581c4409cb7d49f8d5ecb7318d880874af47
azengzz <546800691@qq.com>
2018-11-29 20:00:34 +0800

    up to t292(simple)

--- Leetcode/Simples/T235_DataStructures.cs
+++ Leetcode/Simples/T235_DataStructures.cs
@@ -28,5 +28,289 @@ namespace Leetcode.Simples
         }
 
         #endregion
+
+        #region T237 只给定链表中的某个节点（非最后一个节点），调用这个方法删除它
+
+        //已经知道node不是最后一个节点，所以可以这么做。把node节点的下个节点的值取来，然后删掉下个节点
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+
+        #endregion
+
+        #region T242 判断一个字符串t是否是另一个字符串s的字母异位词
+
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!dict.ContainsKey(s[i]))
+                {
+                    dict.Add(s[i], 1);
+                }
+                else
+                {
+                    dict[s[i]]++;
+                }
+                if (!dict.ContainsKey(t[i]))
+                {
+                    dict.Add(t[i], -1);
+                }
+                else
+                {
+                    dict[t[i]]--;
+                }
+            }
+            var colle = from d in dict where d.Value != 0 select d.Key;
+            if (colle.ToList<char>().Count != 0) return false;
+            return true;
+        }
+
+        #endregion
+
+        #region T257 二叉树的所有路径
+
+        public IList<string> BinaryTreePaths(TreeNode root)
+        {
+            IList<string> result = new List<string>();
+            List<TreeNode> popedNodes = new List<TreeNode>();
+            Stack<TreeNode> stk = new Stack<TreeNode>();
+
+            if (root == null) return result;
+
+            stk.Push(root);
+            while (stk.Count != 0)    //深度优先搜索
+            {
+                TreeNode node = stk.Pop();
+                popedNodes.Add(node);    //出栈的节点放入链表中
+                if (node.left == null && node.right == null)
+                {
+                    result.Add(CreateString(popedNodes));
+                    popedNodes.RemoveAt(popedNodes.Count - 1);
+                }
+                if (node.right != null) stk.Push(node.right);    //先右孩子入栈
+                if (node.left != null) stk.Push(node.left);    //再左孩子入栈
+
+                //如果链表最后一个节点的左右孩子都不在栈中了，说明它的左右孩子都已访问过，它自己也被访问过，那就可从链表中删掉了
+                while (popedNodes.Count > 0 && !stk.Contains(popedNodes[popedNodes.Count-1].left) && !stk.Contains(popedNodes[popedNodes.Count - 1].right))
+                {
+                    popedNodes.RemoveAt(popedNodes.Count - 1);
+                }
+                
+            }
+            return result;
+        }
+
+        private string CreateString(List<TreeNode> popedNodes)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < popedNodes.Count - 1; i++)
+            {
+                sb.Append(popedNodes[i].val.ToString() + "->");
+            }
+            sb.Append(popedNodes[popedNodes.Count - 1].val.ToString());
+            return sb.ToString();
+        }
+
+        #endregion
+
+        #region T263 判断是否丑数
+
+        public bool IsUgly(int num)
+        {
+            if (num < 1) return false;
+
+            while (num > 5)    //1~5都是丑数
+            {
+                if (num % 2 == 0)
+                {
+                    num = num / 2;
+                    continue;
+                }
+                if (num % 3 == 0)
+                {
+                    num = num / 3;
+                    continue;
+                }
+                if (num % 5 == 0)
+                {
+                    num = num / 5;
+                    continue;
+                }
+                return false;
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        #region T268 一个序列从0开始，0,1,2,....,n，共n个数，问其中缺失的数字是多少
+
+        public int MissingNumber(int[] nums)
+        {
+            int len = nums.Length;
+            int expectSum = len * (len + 1) / 2;
+            int serialSum = 0;
+            for (int i = 0; i < len; i++)
+            {
+                serialSum += nums[i];
+            }
+            return expectSum - serialSum;
+        }
+
+        #endregion
+
+        #region T278 第一个错误的版本
+
+        public int FirstBadVersion(int n)
+        {
+            int factor = 0;
+            if (n > int.MaxValue / 2)
+            {
+                if (IsBadVersion(int.MaxValue / 2) == false)
+                {
+                    factor = int.MaxValue / 2;
+                    n -= factor;
+                }
+            }
+
+            int begin = 1, end = n;
+            int mid = 0;
+            bool isBad = false;
+            while (begin < end)
+            {
+                mid = (begin + end) / 2;
+                isBad = IsBadVersion(mid + factor);
+                if (isBad == true)
+                {
+                    end = mid;
+                }
+                else
+                {
+                    if (end - mid == 1) return end + factor;
+                    begin = mid;
+                }
+            }
+            return begin + factor;
+        }
+
+        //这是个API，这里就假设版本号4之后的都是出错的版本
+        bool IsBadVersion(int version)
+        {
+            return version >= 1702766719;
+        }
+
+        //------看看别人的做法。如何避免溢出的
+        //public class Solution : VersionControl
+        //{
+        //    public int FirstBadVersion(int n)
+        //    {
+        //        return getBadVersion(0, n);
+        //    }
+
+        //    private int getBadVersion(int start, int end)
+        //    {
+        //        if (start == end)
+        //        {
+        //            return start;
+        //        }
+        //        int mid = start + (end - start) / 2;
+        //        return IsBadVersion(mid) ? getBadVersion(start, mid) : getBadVersion(mid + 1, end);
+        //    }
+        //}
+
+        //--------非递归版本
+        //public int FirstBadVersion(int n)
+        //{
+        //    int left = 0;
+        //    int right = n;
+        //    int mid = 0;
+        //    while (left != right)
+        //    {
+        //        mid = left + (right - left) / 2;
+        //        if (IsBadVersion(mid))
+        //        {
+        //            right = mid;
+        //        }
+        //        else
+        //        {
+        //            left = mid + 1;
+        //        }
+        //    }
+        //    return left;
+        //}
+
+        #endregion
+
+        #region T283 移动数组中的零到尾部，但保持非零数字的相对顺序。要求不适用额外空间实现，尽量少次数
+
+        public void MoveZeroes(int[] nums)
+        {
+            int slow = 0, fast = 0;
+            while (fast < nums.Length - 1)
+            {
+                ++fast;
+                if (nums[fast] == 0) continue;
+                if (nums[slow] != 0)
+                {
+                    ++slow;
+                }
+                if (fast > slow)
+                {
+                    nums[slow] = nums[fast];
+                }
+            }
+
+            slow++;
+            while (slow < nums.Length)
+            {
+                if (nums[slow] != 0) nums[slow] = 0;
+                slow++;
+            }
+        }
+
+        #endregion
+
+        #region T290 判断给定的字符串是否复合给定的模式
+
+        public bool WordPattern(string pattern, string str)
+        {        
+            string[] words = str.Split(' ');
+
+            if (pattern.Length != words.Length) return false;
+
+            Dictionary<char, string> dict = new Dictionary<char, string>();    //pattern每个字符和words每个词作为一对
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (!dict.ContainsKey(pattern[i]))
+                {
+                    if (dict.ContainsValue(words[i])) return false;
+                    dict.Add(pattern[i], words[i]);
+                }
+                else
+                {
+                    if (dict[pattern[i]] != words[i]) return false;
+                }
+            }
+            return true;
+        }
+
+        #endregion
+
+        #region T292 Nim游戏
+        //你和你的朋友，两个人一起玩 Nim游戏：桌子上有一堆石头，每次你们轮流拿掉 1 - 3 块石头。 拿掉最后一块石头的人就是获胜者。你作为先手。
+        //你们是聪明人，每一步都是最优解。 编写一个函数，来判断你是否可以在给定石头数量的情况下赢得游戏。
+        public bool CanWinNim(int n)
+        {
+            return (n % 4 != 0);
+        }
+
+        #endregion
     }
 }
--- LeetcodeTests/Simples/T235_DataStructuresTests.cs
+++ LeetcodeTests/Simples/T235_DataStructuresTests.cs
@@ -13,6 +13,8 @@ namespace Leetcode.Simples.Tests
     {
         T235_DataStructures t235 = new T235_DataStructures();
 
+        #region T235 tests : 二叉搜索树的最近公共祖先
+
         [TestMethod()]
         public void LowestCommonAncestorTest_1()
         {
@@ -49,5 +51,198 @@ namespace Leetcode.Simples.Tests
             Assert.IsTrue(expect.val == t235.LowestCommonAncestor(tree, p, q).val);
         }
 
+        #endregion
+
+        #region T242 tests : 判断是否字母异位词
+
+        [TestMethod()]
+        public void IsAnagramTest_1()
+        {
+            string s = "anagram", t = "nagaram";
+            Assert.IsTrue(true == t235.IsAnagram(s, t));
+        }
+
+
+        [TestMethod()]
+        public void IsAnagramTest_2()
+        {
+            string s = "anagram", t = "nagarab";
+            Assert.IsTrue(false == t235.IsAnagram(s, t));
+        }
+
+        [TestMethod()]
+        public void IsAnagramTest_3()
+        {
+            string s = "aaabb", t = "aabbb";
+            Assert.IsTrue(false == t235.IsAnagram(s, t));
+        }
+
+        #endregion
+
+        #region T257 tests : 二叉树的所有路径
+
+        [TestMethod()]
+        public void BinaryTreePathsTest_1()
+        {
+            object[] nodes = { 6, 2, 8, 0, 4, 7, 9, null, null, 3, 5 };
+            TreeNode tree = TreeHelper.CreateBinaryTreeByArray(nodes);
+            IList<string> result = t235.BinaryTreePaths(tree);
+            Assert.IsTrue(result != null);
+        }
+
+        #endregion
+
+        #region T263 tests : 丑数判断
+
+        [TestMethod()]
+        public void IsUglyTest_1()
+        {
+            Assert.IsTrue(true == t235.IsUgly(10));
+        }
+
+        [TestMethod()]
+        public void IsUglyTest_2()
+        {
+            Assert.IsTrue(false == t235.IsUgly(456));
+        }
+
+        [TestMethod()]
+        public void IsUglyTest_3()
+        {
+            Assert.IsTrue(false == t235.IsUgly(19456416));
+        }
+
+        #endregion
+
+        #region T268 tests : 求缺失的数字
+
+        [TestMethod()]
+        public void MissingNumberTest_1()
+        {
+            int[] serial = { 3, 0, 1 };
+            Assert.IsTrue(2 == t235.MissingNumber(serial));
+        }
+
+        [TestMethod()]
+        public void MissingNumberTest_2()
+        {
+            int[] serial = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+            Assert.IsTrue(8 == t235.MissingNumber(serial));
+        }
+
+        #endregion
+
+        #region T278 tests : 查找第一个错误的版本号
+
+        //[TestMethod()]
+        //public void FirstBadVersionTest_1()
+        //{
+        //    Assert.IsTrue(4 == t235.FirstBadVersion(5));
+        //}
+
+        //[TestMethod()]
+        //public void FirstBadVersionTest_2()
+        //{
+        //    Assert.IsTrue(4 == t235.FirstBadVersion(15));
+        //}
+
+        //[TestMethod()]
+        //public void FirstBadVersionTest_3()
+        //{
+        //    Assert.IsTrue(4 == t235.FirstBadVersion(7));
+        //}
+
+        //[TestMethod()]
+        //public void FirstBadVersionTest_3()
+        //{
+        //    Assert.IsTrue(1702766719 == t235.FirstBadVersion(2126753390));
+        //}
+
+        #endregion
+
+        #region T283 tests : 移动数组中的0到数组尾部
+
+        [TestMethod()]
+        public void MoveZeroesTest_1()
+        {
+            int[] nums = { 1, 0, 2, 4, 0, 3 };
+            int[] expect = { 1, 2, 4, 3, 0, 0 };
+            t235.MoveZeroes(nums);
+            Assert.IsTrue(CompareHelper.CompareArrays(nums, expect));
+        }
+
+        [TestMethod()]
+        public void MoveZeroesTest_2()
+        {
+            int[] nums = { 0, 3, 2, 4, 0, 1, 5, 0, 6 };
+            int[] expect = { 3, 2, 4, 1, 5, 6, 0, 0, 0 };
+            t235.MoveZeroes(nums);
+            Assert.IsTrue(CompareHelper.CompareArrays(nums, expect));
+        }
+
+        [TestMethod()]
+        public void MoveZeroesTest_3()
+        {
+            int[] nums = { 5, 4, 3, 2, 1 };
+            int[] expect = { 5, 4, 3, 2, 1 };
+            t235.MoveZeroes(nums);
+            Assert.IsTrue(CompareHelper.CompareArrays(nums, expect));
+        }
+
+        [TestMethod()]
+        public void MoveZeroesTest_4()
+        {
+            int[] nums = { 3 };
+            int[] expect = { 3 };
+            t235.MoveZeroes(nums);
+            Assert.IsTrue(CompareHelper.CompareArrays(nums, expect));
+        }
+
+        [TestMethod()]
+        public void MoveZeroesTest_5()
+        {
+            int[] nums = { 0, 0, 0, 0, 0, 0 };
+            int[] expect = { 0, 0, 0, 0, 0, 0 };
+            t235.MoveZeroes(nums);
+            Assert.IsTrue(CompareHelper.CompareArrays(nums, expect));
+        }
+
+        #endregion
+
+        #region tests : 判断字符串str是否符合模式pattern
+
+        [TestMethod()]
+        public void WordPatternTest_1()
+        {
+            string str = "dog cat cat dog";
+            string pattern = "abba";
+            Assert.IsTrue(true == t235.WordPattern(pattern, str));
+        }
+
+        [TestMethod()]
+        public void WordPatternTest_2()
+        {
+            string str = "dog cat cat fish";
+            string pattern = "abba";
+            Assert.IsTrue(false == t235.WordPattern(pattern, str));
+        }
+
+        [TestMethod()]
+        public void WordPatternTest_3()
+        {
+            string str = "dog cat cat dog";
+            string pattern = "aaaa";
+            Assert.IsTrue(false == t235.WordPattern(pattern, str));
+        }
+
+        [TestMethod()]
+        public void WordPatternTest_4()
+        {
+            string str = "dog dog dog dog";
+            string pattern = "abba";
+            Assert.IsTrue(false == t235.WordPattern(pattern, str));
+        }
+
+        #endregion
     }
 }
