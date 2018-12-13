1d11c79d88ff91994a499ba5fa627b2910cc042a
azengzz <546800691@qq.com>
2018-12-13 22:51:51 +0800

    up to t501. 5 subjects today

---
+++ Leetcode/Simples/T485_MathProblems.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Simples
+{
+    public class T485_MathProblems
+    {
+        public T485_MathProblems() { }
+
+        #region T485 给定一个二进制数组，计算其中最大了连续1的个数
+
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int count = 0, res = 0;
+            for (int i = 0; i <= nums.Length; i++)
+            {
+                if (i == nums.Length || nums[i] == 0)
+                {
+                    if (count > res) res = count;
+                    count = 0;
+                }
+                else
+                    count++;
+            }
+            return res;
+        }
+
+        #endregion
+
+        #region T492 构造矩形
+
+        public int[] ConstructRectangle(int area)
+        {
+            int length = (int)Math.Sqrt(area);
+            while (area % length != 0)
+            {
+                length++;
+            }
+            int width = area / length;
+            return new int[] { length, width };
+        }
+
+        #endregion
+
+        #region T496 寻找下一个更大的元素
+
+        public int[] NextGreaterElement(int[] findNums, int[] nums)
+        {
+            //遍历nums，栈为空时将当前元素入栈；栈不空时，若当前元素大于栈顶，则出栈，将出栈值与当前元素构成键值对
+            Stack<int> stk = new Stack<int>();
+            Dictionary<int, int> nextLargerDict = new Dictionary<int, int>();
+            int j = 0;
+            for (j = 0; j < nums.Length; j++)
+            {
+                while (stk.Count > 0 && nums[j] > stk.Peek())
+                {
+                    nextLargerDict.Add(stk.Pop(), nums[j]);
+                }                 
+                stk.Push(nums[j]);
+            }
+
+            int[] res = new int[findNums.Length];
+            for (int ii = 0; ii < findNums.Length; ii++)
+            {
+                if (nextLargerDict.ContainsKey(findNums[ii]))
+                    res[ii] = nextLargerDict[findNums[ii]];
+                else
+                    res[ii] = -1;
+            }
+            return res;
+        }
+
+        #endregion
+
+        #region T500 判断单词是否在键盘上的同一行
+
+        public string[] FindWords(string[] words)
+        {
+            List<string> list = new List<string> { "qwertyuiop", "asdfghjkl", "zxcvbnm" };
+            List<string> res = new List<string>();
+            foreach (string w in words)
+            {
+                List<char> tmp = w.ToLower().ToList<char>();
+
+                var colle = from li in list where tmp.All(c => li.Contains(c)) select w;
+                if (colle.ToArray().Length > 0)
+                    res.Add(w);
+                
+            }
+            return res.ToArray();
+        }
+
+        #endregion
+
+        #region T501 二叉搜索树中的众数        
+
+        public int[] FindMode(TreeNode root)
+        {
+            int currentCount = 0;
+            int maxCount = 0;
+            int lastPriorVal = 0;
+            List<int> res = new List<int>();
+
+            InOrderTraversal(root, ref currentCount, ref maxCount, ref lastPriorVal, res);
+            return res.ToArray();
+            
+        }
+
+        private void InOrderTraversal(TreeNode root, ref int currentCount, ref int maxCount, ref int lastPriorVal, List<int> res)
+        {
+            if (root == null) return ;           
+
+            InOrderTraversal(root.left, ref currentCount, ref maxCount, ref lastPriorVal, res);
+            currentCount++;
+            if (lastPriorVal != root.val)
+            {
+                lastPriorVal = root.val;
+                currentCount = 1;
+            }
+          
+            if (currentCount > maxCount)
+            {
+                maxCount = currentCount;
+                res.Clear();
+                res.Add(root.val);
+            }
+            else if (currentCount == maxCount)
+            {
+                res.Add(root.val);
+            }
+            InOrderTraversal(root.right, ref currentCount, ref maxCount, ref lastPriorVal, res);
+        }
+
+        #endregion
+    }
+}
---
+++ LeetcodeTests/Simples/T485_MathProblemsTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.Simples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Simples.Tests
+{
+    [TestClass()]
+    public class T485_MathProblemsTests
+    {
+        T485_MathProblems t485 = new T485_MathProblems();
+
+        #region T485 tests : 求二进制数组中最大连续1的个数
+
+        [TestMethod()]
+        public void FindMaxConsecutiveOnesTest_1()
+        {
+            Assert.IsTrue(5 == t485.FindMaxConsecutiveOnes(new int[] { 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1 }));
+        }
+
+        #endregion
+
+        #region T492 tests : 构造矩形
+
+        [TestMethod()]
+        public void ConstructRectangleTest_1()
+        {
+            int[] expect = { 11, 2 };
+            Assert.IsTrue(true == CompareHelper.CompareArrays(expect, t485.ConstructRectangle(22)));
+        }
+
+        #endregion
+
+        #region T496 tests : 寻找下一个更大的元素
+
+        [TestMethod()]
+        public void NextGreaterElementTest_1()
+        {
+            int[] expect = { -1, 3, -1 };
+            Assert.IsTrue(CompareHelper.CompareArrays(expect,
+                t485.NextGreaterElement(new int[] { 4, 1, 2 }, new int[] { 1, 3, 4, 2 })));
+        }
+
+        [TestMethod()]
+        public void NextGreaterElementTest_2()
+        {
+            int[] expect = { -1, 5, -1, 5 };
+            Assert.IsTrue(CompareHelper.CompareArrays(expect,
+                t485.NextGreaterElement(new int[] { 5, 3, 6, 4 }, new int[] { 7, 8, 9, 2, 10, 6, 1, 4, 3, 5 })));
+        }
+
+        #endregion
+
+        #region T500 tests : 判断单词是否在键盘上的同一行
+
+        [TestMethod()]
+        public void FindWordsTest_1()
+        {
+            string[] words = { "Hello", "Alaska", "Dad", "Peace" };
+            string[] expect = { "Alaska", "Dad" };
+
+            Assert.IsTrue(CompareHelper.CompareArrays<string>(expect, t485.FindWords(words)));
+        }
+
+        #endregion
+
+        #region T501 tests : 二叉搜索树中的众数
+
+        [TestMethod()]
+        public void FindModeTest_1()
+        {
+            TreeNode tree = TreeHelper.CreateBinaryTreeByArray(new object[] { 1, null, 2, 2 });
+            int[] expect = { 2 };
+            Assert.IsTrue(CompareHelper.CompareArrays(expect, t485.FindMode(tree)));
+        }
+
+        [TestMethod()]
+        public void FindModeTest_2()
+        {
+            TreeNode tree = TreeHelper.CreateBinaryTreeByArray(new object[] { 3, 2, 3, 1, 2, null, 4 });
+            int[] expect = { 2, 3 };
+            Assert.IsTrue(CompareHelper.CompareArrays(expect, t485.FindMode(tree)));
+        }
+
+        #endregion
+    }
+}
