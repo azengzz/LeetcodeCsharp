f111593f2199662c814ed242fe97844f5a9f28af
azengzz <546800691@qq.com>
2019-01-01 14:36:57 +0800

    up to m02 (medium)

---
+++ Leetcode/Mediums/Medium_T002.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Mediums
+{
+    public class Medium_T002
+    {
+        public Medium_T002() { }
+
+        #region T02 两数相加
+
+        private class AddTwoHelper
+        {
+            public AddTwoHelper(int carry, ListNode ptr)
+            {
+                Carry = carry;
+                CurrentPtr = ptr;
+            }
+
+            public int Carry { get; set; }
+            public ListNode CurrentPtr { get; set; }
+        }
+
+        //不能用普通的整数类型来存储结果，因为这样用链表表示的数字可能是天文数字
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode head = new ListNode(int.MinValue);
+            ListNode current = head;
+
+            AddTwoHelper helper = new AddTwoHelper(0, current);
+            
+            while (l1 != null && l2 != null)
+            {
+                AddTwoNumberInList(l1, l2, helper);
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            //链表长度不一样时，把长的多出来那截加上
+            while (l1 != null)
+            {
+                AddTwoNumberInList(l1, null, helper);
+                l1 = l1.next;
+            }
+
+            while (l2 != null)
+            {
+                AddTwoNumberInList(l2, null, helper);
+                l2 = l2.next;
+            }
+
+            if (helper.Carry != 0)
+            {
+                helper.CurrentPtr.next = new ListNode(helper.Carry);
+            }
+
+            return head.next;
+        }
+
+        private void AddTwoNumberInList(ListNode node1, ListNode node2, AddTwoHelper helper)
+        {
+            int sum = 0;
+
+            if (node2 != null)
+            {
+                sum = node1.val + node2.val + helper.Carry;
+            }
+            else
+            {
+                sum = node1.val + helper.Carry;
+            }
+
+            helper.Carry = sum / 10;
+            sum = sum % 10;
+            helper.CurrentPtr.next = new ListNode(sum);
+            helper.CurrentPtr = helper.CurrentPtr.next;
+        }
+
+        #endregion
+    }
+}
---
+++ LeetcodeTests/Mediums/Medium_T002Tests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leetcode.Mediums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Mediums.Tests
+{
+    [TestClass()]
+    public class Medium_T002Tests
+    {
+        public Medium_T002 m02 = new Medium_T002();
+
+        #region M02 tests : 两数相加
+
+        [TestMethod()]
+        public void AddTwoNumbersTest_1()
+        {
+            int[] nums1 = { 2, 4, 3 };
+            int[] nums2 = { 5, 6, 4 };
+            ListNode lst1 = ListNodeHelper.CreateLinkedListByArray(nums1);
+            ListNode lst2 = ListNodeHelper.CreateLinkedListByArray(nums2);
+
+            int[] expect = { 7, 0, 8 };
+            int[] res = ListNodeHelper.GetElementsFromLinkedList(m02.AddTwoNumbers(lst1, lst2));
+
+            Assert.IsTrue(CompareHelper.CompareArrays(expect, res));
+        }
+
+        [TestMethod()]
+        public void AddTwoNumbersTest_2()
+        {
+            int[] nums1 = { 6, 7, 8, 9, 9 };
+            int[] nums2 = { 1, 2, 3 };
+            ListNode lst1 = ListNodeHelper.CreateLinkedListByArray(nums1);
+            ListNode lst2 = ListNodeHelper.CreateLinkedListByArray(nums2);
+
+            int[] expect = { 7, 9, 1, 0, 0, 1 };
+            int[] res = ListNodeHelper.GetElementsFromLinkedList(m02.AddTwoNumbers(lst1, lst2));
+
+            Assert.IsTrue(CompareHelper.CompareArrays(expect, res));
+        }
+
+        #endregion
+    }
+}
