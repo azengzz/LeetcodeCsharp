04a490b6b2700218978cdc9fb4ee60ba22897c27
azengzz <546800691@qq.com>
2019-01-31 20:44:03 +0800

    up to T031 (medium)

--- Leetcode/Mediums/Medium_T002.cs
+++ Leetcode/Mediums/Medium_T002.cs
@@ -208,5 +208,580 @@ namespace Leetcode.Mediums
         }
 
         #endregion
+
+        #region T08 字符串转换整数
+
+        public int MyAtoi(string str)
+        {
+            if (str == null || str.Length == 0)
+            {
+                return 0;
+            }
+
+            if (str[0] != ' ' && str[0] != '+' && str[0] != '-'
+            && !(str[0] >= '0' && str[0] <= '9'))
+            {
+                return 0;
+            }
+
+            int index = 0;
+            while (index < str.Length && str[index] == ' ')
+            {
+                ++index;
+            }
+
+            long res = 0;
+            bool isPositive = true;
+
+            if (index < str.Length && (str[index] == '-' || str[index] == '+'))
+            {
+                isPositive = str[index] == '-' ? false : true;
+                ++index;
+            }
+
+            for (int i = index; i < str.Length; ++i)
+            {
+                if (str[i] >= '0' && str[i] <= '9')
+                {
+                    res *= 10;
+                    res += (long)(str[i] - '0');
+
+                    if (res > (long)int.MaxValue)
+                    {
+                        if (isPositive)
+                        {
+                            return int.MaxValue;
+                        }
+                        else
+                        {
+                            if (res > (long)int.MaxValue + 1)
+                            {
+                                return int.MinValue;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    return isPositive ? (int)res : (int)res * -1;
+                }
+            }
+
+            return isPositive ? (int)res : (int)res * -1;
+        }
+
+        #endregion
+
+        #region T11 盛水最多的容器
+
+        public int MaxArea(int[] height)
+        {
+            int max = int.MinValue;
+            int left = 0;
+            int right = height.Length - 1;
+
+            while (left < right)
+            {
+                int lower = height[left] <= height[right] ? left : right;
+                int width = right - left;
+
+                int area = height[lower] * width;
+
+                max = area > max ? area : max;
+
+                if (lower == left)
+                {
+                    ++left;
+                }
+                else
+                {
+                    --right;
+                }
+            }
+
+            return max;
+        }
+
+        #endregion
+
+        #region T12 整数转换为罗马数字
+
+        public string IntToRoman(int num)
+        {
+
+            StringBuilder res = new StringBuilder();
+
+            if (num >= 1000)
+            {
+                int digit = num / 1000;
+
+                res.Append(ConvertHelper(digit, "", "", "M"));
+
+                num %= 1000;
+            }
+
+            if (num >= 100 && num <= 999)
+            {
+                int digit = num / 100;
+
+                res.Append(ConvertHelper(digit, "M", "D", "C"));
+
+                num %= 100;
+            }
+
+            if (num >= 10 && num <= 99)
+            {
+                int digit = num / 10;
+
+                res.Append(ConvertHelper(digit, "C", "L", "X"));
+
+                num %= 10;
+            }
+
+            res.Append(ConvertHelper(num, "X", "V", "I"));
+
+            return res.ToString();
+        }
+
+        private string ConvertHelper(int digit, string ten, string five, string one)
+        {
+            string res = "";
+
+            if (digit == 9)
+            {
+                res += one + ten;
+            }
+            else if (digit == 4)
+            {
+                res += one + five;
+            }
+            else
+            {
+                if (digit >= 5)
+                {
+                    res += five;
+                    digit -= 5;
+                }
+                while (digit > 0)
+                {
+                    res += one;
+                    --digit;
+                }
+            }
+
+            return res;
+        }
+
+        #endregion
+
+        #region T15 三数之和
+
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+
+            if (nums.Length < 3)
+            {
+                return res;
+            }
+
+            Array.Sort(nums);
+
+            for (int left = 0; left < nums.Length; ++left)
+            {
+                if (left != 0 && nums[left] == nums[left - 1])    //跳过重复项
+                    continue;
+
+                int mid = left + 1;
+                int right = nums.Length - 1;
+
+                while (mid < right)
+                {
+                    if (nums[left] + nums[mid] + nums[right] == 0)
+                    {
+                        res.Add(new List<int> { nums[left], nums[mid], nums[right] });
+
+                        //忽略重复项
+                        ++mid;
+                        --right;
+
+                        while (mid < right && nums[mid] == nums[mid - 1])
+                        {
+                            ++mid;
+                        }
+                        while (mid < right && nums[right] == nums[right + 1])
+                        {
+                            --right;
+                        }
+                    }
+                    else if (nums[left] + nums[mid] + nums[right] > 0)
+                    {
+                        --right;
+                    }
+                    else
+                    {
+                        ++mid;
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        #endregion
+
+        #region T16 最接近的三数之和
+
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            if (nums.Length < 3)
+            {
+                return 0;
+            }
+
+            Array.Sort(nums);
+
+            int minDistance = int.MaxValue;
+            int res = 0;
+
+            for (int i = 0; i < nums.Length - 2; ++i)
+            {
+                int left = i + 1;
+                int right = nums.Length - 1;
+
+                int sum = 0;
+
+                while (left < right)
+                {
+                    sum = nums[left] + nums[right] + nums[i];
+
+                    int distance = Math.Abs(target - sum);
+
+                    if (minDistance > distance)
+                    {
+                        minDistance = distance;
+                        res = sum;
+                    }
+
+                    if (sum == target)
+                    {
+                        return res;
+                    }
+                    else if (sum < target)
+                    {
+                        ++left;
+                    }
+                    else
+                    {
+                        --right;
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        #endregion
+
+        #region T17 电话号码的字母组合
+
+        public IList<string> LetterCombinations(string digits)
+        {
+            Dictionary<char, List<char>> dict = new Dictionary<char, List<char>>
+            {
+                {'0', new List<char> {' '}},
+                {'1', new List<char> {'*'}},
+                {'2', new List<char> {'a', 'b', 'c'}},
+                {'3', new List<char> {'d', 'e', 'f'}},
+                {'4', new List<char> {'g', 'h', 'i'}},
+                {'5', new List<char> {'j', 'k', 'l'}},
+                {'6', new List<char> {'m', 'n', 'o'}},
+                {'7', new List<char> {'p', 'q', 'r', 's'}},
+                {'8', new List<char> {'t', 'u', 'v'}},
+                {'9', new List<char> {'w', 'x', 'y', 'z'}},
+            };
+
+            if (digits == null || digits.Length == 0)
+            {
+                return new List<string>();
+            }
+
+            Queue<string> queue = new Queue<string>();
+
+            queue.Enqueue("");
+
+            for (int i = 0; i < digits.Length; ++i)
+            {
+                int count = queue.Count;
+                List<char> letters = dict[digits[i]];
+
+                while (count-- > 0)
+                {
+                    string str = queue.Dequeue();
+
+                    foreach (char ch in letters)
+                    {
+                        queue.Enqueue(str + ch);
+                    }
+                }
+            }
+
+            return queue.ToList();
+        }
+
+        #endregion
+
+        #region T18 四数之和
+
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+
+            if (nums.Length < 4)
+            {
+                return res;
+            }
+
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 3; ++i)
+            {
+                for (int j = i + 1; j < nums.Length - 2; ++j)
+                {
+                    int left = j + 1;
+                    int right = nums.Length - 1;
+                    int sum = 0;
+
+                    while (left < right)
+                    {
+                        sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                        if (sum == target)
+                        {
+                            List<int> found = new List<int> { nums[i], nums[j], nums[left], nums[right] };
+                            res.Add(found);
+
+                            ++left;
+                            --right;
+
+                            //跳过相同项
+                            while (left < right && found[2] == nums[left])
+                            {
+                                ++left;
+                            }
+                            while (left < right && found[3] == nums[right])
+                            {
+                                --right;
+                            }
+                        }
+                        else if (sum < target)
+                        {
+                            ++left;
+                        }
+                        else
+                        {
+                            --right;
+                        }
+                    }
+
+                    while (j < nums.Length - 2 && nums[j] == nums[j + 1])    //跳过相同项
+                    {
+                        ++j;
+                    }
+                }
+
+                while (i < nums.Length - 3 && nums[i] == nums[i + 1])    //跳过相同项
+                {
+                    ++i;
+                }
+            }
+
+            return res;
+        }
+
+        #endregion
+
+        #region T19 删除链表的倒数第N个节点
+
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (head == null || n == 0)
+            {
+                return head;
+            }
+
+            ListNode fast = head;
+            int count = n;
+
+            while (count > 0 && fast != null)
+            {
+                --count;
+                fast = fast.next;
+            }
+
+            if (fast == null)
+            {
+                return head.next;
+            }
+
+            ListNode slow = head;
+
+            while (fast.next != null)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+            slow.next = slow.next.next;
+
+            return head;
+        }
+
+        #endregion
+
+        #region T24 两两交换链表中的节点
+
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null)
+            {
+                return head;
+            }
+
+            ListNode pNew = new ListNode(0);
+            pNew.next = head;
+
+            ListNode pre = pNew;
+
+            ListNode ptr1 = head;
+            ListNode ptr2 = ptr1.next;
+
+            while (ptr1 != null)
+            {
+                if (ptr2 == null)
+                {
+                    return pNew.next;
+                }
+
+                ptr1.next = ptr2.next;
+                ptr2.next = ptr1;
+
+                pre.next = ptr2;
+                pre = ptr1;
+
+                ptr1 = ptr1.next;
+
+                if (ptr1 == null)
+                {
+                    return pNew.next;
+                }
+
+                ptr2 = ptr1.next;
+            }
+
+            return pNew.next;
+        }
+
+        #endregion
+
+        #region T29 两数相除
+
+        public int Divide(int dividend, int divisor)
+        {
+
+            bool isNegative = (dividend < 0) ^ (divisor < 0);
+
+            long ldividend = Math.Abs((long)dividend);
+            long ldivisor = Math.Abs((long)divisor);
+
+            if (ldivisor == 0)
+            {
+                return int.MaxValue;
+            }
+
+            if (ldividend == 0 || (ldividend < ldivisor))
+            {
+                return 0;
+            }
+
+            long result = Divide(ldividend, ldivisor);
+
+            if (result > int.MaxValue)
+            {
+                return isNegative ? int.MinValue : int.MaxValue;
+            }
+
+            return (int)(isNegative ? -result : result);
+
+        }
+
+        private long Divide(long dividend, long divisor)
+        {
+            if (dividend < divisor)
+            {
+                return 0;
+            }
+
+            long sum = divisor;
+            long result = 1;
+
+            while (dividend >= (sum << 1))
+            {
+                sum <<= 1;
+                result += result;
+            }
+
+            return result + Divide(dividend - sum, divisor);
+        }
+
+        #endregion
+
+        #region T31 下一个排列
+
+        public void NextPermutation(int[] nums)
+        {
+
+            int ptr = nums.Length - 2;
+
+            while (ptr >= 0 && nums[ptr + 1] <= nums[ptr])
+            {
+                --ptr;
+            }
+
+            if (ptr >= 0)
+            {
+                int next = nums.Length - 1;
+
+                while (next >= 0 && nums[next] <= nums[ptr])
+                {
+                    --next;
+                }
+
+                SwapNums(nums, ptr, next);
+            }
+
+            ReverseNums(nums, ptr + 1);
+        }
+
+        private void SwapNums(int[] nums, int aIdx, int bIdx)
+        {
+            int tmp = nums[aIdx];
+            nums[aIdx] = nums[bIdx];
+            nums[bIdx] = tmp;
+        }
+
+        private void ReverseNums(int[] nums, int start)
+        {
+            int end = nums.Length - 1;
+
+            while (start < end)
+            {
+                SwapNums(nums, start, end);
+                ++start;
+                --end;
+            }
+        }
+
+        #endregion
     }
 }
--- LeetcodeTests/Mediums/Medium_T002Tests.cs
+++ LeetcodeTests/Mediums/Medium_T002Tests.cs
@@ -100,5 +100,116 @@ namespace Leetcode.Mediums.Tests
         }
 
         #endregion
+
+        #region T08 tests : 字符串转换整数
+
+        [TestMethod()]
+        public void MyAtoiTest_1()
+        {
+            Assert.IsTrue(42 == m02.MyAtoi("42"));
+        }
+
+        [TestMethod()]
+        public void MyAtoiTest_2()
+        {
+            Assert.IsTrue(-42 == m02.MyAtoi("     -42"));
+        }
+
+        [TestMethod()]
+        public void MyAtoiTest_3()
+        {
+            Assert.IsTrue(4193 == m02.MyAtoi("4193 with words"));
+        }
+
+        [TestMethod()]
+        public void MyAtoiTest_4()
+        {
+            Assert.IsTrue(0 == m02.MyAtoi("words and 987"));
+        }
+
+        [TestMethod()]
+        public void MyAtoiTest_5()
+        {
+            Assert.IsTrue(-2147483648 == m02.MyAtoi("-91283472332"));
+        }
+
+        [TestMethod()]
+        public void MyAtoiTest_6()
+        {
+            Assert.IsTrue(2147483647 == m02.MyAtoi("91283472332"));
+        }
+
+        [TestMethod()]
+        public void MyAtoiTest_7()
+        {
+            Assert.IsTrue(4 == m02.MyAtoi("+4 2"));
+        }
+
+        [TestMethod()]
+        public void MyAtoiTest_8()
+        {
+            Assert.IsTrue(0 == m02.MyAtoi(" "));
+        }
+
+        #endregion
+
+        #region T11 tests : 盛水最多的容器
+
+        [TestMethod()]
+        public void MaxAreaTest_1()
+        {
+            int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            Assert.IsTrue(49 == m02.MaxArea(height));
+        }
+
+        [TestMethod()]
+        public void MaxAreaTest_2()
+        {
+            int[] height = { 1, 1 };
+            Assert.IsTrue(1 == m02.MaxArea(height));
+        }
+
+        [TestMethod()]
+        public void MaxAreaTest_3()
+        {
+            int[] height = { 10, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 7 };
+            Assert.IsTrue(161 == m02.MaxArea(height));
+        }
+
+        #endregion
+
+        #region T12 tests : 整数转换罗马数字
+
+        [TestMethod()]
+        public void IntToRomanTest_1()
+        {
+            Assert.IsTrue("III" == m02.IntToRoman(3));
+        }
+
+        [TestMethod()]
+        public void IntToRomanTest_2()
+        {
+            Assert.IsTrue("IV" == m02.IntToRoman(4));
+        }
+
+        [TestMethod()]
+        public void IntToRomanTest_3()
+        {
+            Assert.IsTrue("IX" == m02.IntToRoman(9));
+        }
+
+        [TestMethod()]
+        public void IntToRomanTest_4()
+        {
+            Assert.IsTrue("LVIII" == m02.IntToRoman(58));
+        }
+
+        [TestMethod()]
+        public void IntToRomanTest_5()
+        {
+            Assert.IsTrue("MCMXCIV" == m02.IntToRoman(1994));
+        }
+
+        #endregion
     }
 }
